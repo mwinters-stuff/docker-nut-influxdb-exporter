b7be60837ce0f594cd420893dff501ee5263966c
Mathew Winters <mathew@winters.org.nz>
2021-03-14 11:25:34 +1300

    fix script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-git clone --depth=1 --single-branch --branch ${BRANCH} https://github.com/mihai-cindea/nut-influxdb-exporter.git
+BRANCH=$1
+git clone --depth=1 --single-branch --branch ${BRANCH} https://github.com/mihai-cindea/nut-influxdb-exporter.git || exit 1
 cd nut-influxdb-exporter || exit 1
 
 VER=$(TZ="utc" date -Iseconds)
