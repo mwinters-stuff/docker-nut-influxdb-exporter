d0d0c684ad8ba4db22f85e699d70406bcfe6da57
Mathew Winters <mathew@winters.org.nz>
2021-03-14 11:29:56 +1300

    tag change

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ BRANCH=$1
 git clone --depth=1 --single-branch --branch ${BRANCH} https://github.com/mihai-cindea/nut-influxdb-exporter.git || exit 1
 cd nut-influxdb-exporter || exit 1
 
-VER=$(TZ="utc" date -Iseconds)
+VER=$(date -u '+%Y%m%d%H%M%S')
 echo "date_version=${VER}"
