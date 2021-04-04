3e44caa8002e7fcac31fdd17f78d67a1b97bda66
Mathew Winters <mathew@winters.org.nz>
2021-04-04 13:36:44 +1200

    Change to own updated repo

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BRANCH=$1
-git clone --depth=1 --single-branch --branch ${BRANCH} https://github.com/mihai-cindea/nut-influxdb-exporter.git || exit 1
+git clone --depth=1 --single-branch --branch ${BRANCH} https://github.com/mwinters-stuff/nut-influxdb-exporter.git || exit 1
 cd nut-influxdb-exporter || exit 1
 
 VER=$(date -u '+%Y%m%d%H%M%S')
